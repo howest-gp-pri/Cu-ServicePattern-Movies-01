b09119a08aead59eee778ebac9cc00ed62754603
Miledi75 <mileto.di.marco@howest.be>
2023-10-06 14:22:22 +0200

    Move interfaces to correct folder

--- Cu-ServicePattern-Movies-01.Core/Services/FileService.cs
+++ Cu-ServicePattern-Movies-01.Core/Services/FileService.cs
@@ -1,5 +1,4 @@
-﻿
-using Cu_ServicePattern_Movies_01.Core.Interfaces;
+﻿using Cu_ServicePattern_Movies_01.Core.Services.Interfaces.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
